444ec1f365493b44bbfd2d58bfbc0b0e8ebe2821
phazor <tristan.r.read@gmail.com>
2017-01-07 14:22:35 +0100

    initial commit

---
+++ analog_input/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace analog_input
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            AnalogInput dial = new AnalogInput(Cpu.AnalogChannel.ANALOG_0);
+
+            while (true)
+            {
+                Thread.Sleep(100);
+                Debug.Print(dial.Read().ToString());
+            };
+        }
+    }
+}
